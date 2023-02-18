0b2589c2b873864bfecce19763d71ce457f43b80
infandis <svetlana290699@gmail.com>
2023-02-18 17:33:28 +0200

    made the second point

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let string1 = prompt ("please enter the first line");
-let string2 = prompt ("please enter the second line");
-let string3 = prompt ("please enter the third line");
-alert (string1.concat( string2 + string3));
+let a = 54321;
+let b = a.toString();
+let c = b.split([]);
+let d = c.join(" ");
+console.log(d);
+
